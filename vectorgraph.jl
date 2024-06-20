0b45598a6693e67859c04ce48dcd220d678a54c5
Gaurav <gaurav.sablok@uni-potsdam.de>
2024-06-20 13:45:13 +0200

    Update vectorgraph.jl

--- vectorgraph.jl
+++ vectorgraph.jl
@@ -4,3 +4,14 @@
 # Date 2024-6-20
 
 # vector graphics implementation for making the metabolic maps
+
+function(reactiontype, stoichiometry)
+  if !reactiontype && !stoichoiometry 
+    println("vector graphics cant be made for this component")
+  if reactiontype && stoichiometry
+      inmetabolite = Vector{Int16}
+      outmetabolite = Vector{Int16]
+   if reactiontype == "reversible"
+        
+      
+  
